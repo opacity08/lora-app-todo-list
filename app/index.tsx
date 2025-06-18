05a016a57526314489ef4a68bfcbb349080d59c2
Niko <mykola@catchmedia.no>
2025-06-18 16:20:58 +0200

    can add new task

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import {View, Text, FlatList, StyleSheet} from 'react-native';
+import { useState } from 'react';
+import { View, Text, FlatList, StyleSheet, TextInput, Button } from 'react-native';
 
 type Task = {
   id: string;
@@ -7,15 +8,49 @@ type Task = {
 };
 
 export default function HomeScreen() {
-  const tasks: Task[] = [
-    {id: '1', title: 'Buy a bread', description: 'I need to fullcorn bread'},
-    {id: '2', title: 'Call mama'},
-    {id: '3', title: 'Finish project'}
-  ];
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+
+  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [newTaskDesc, setNewTaskDesc] = useState('');
+
+  function addTask() {
+    if (!newTaskTitle.trim()) return;
+
+    const newTask = {
+      id: String(Date.now()),
+      title: newTaskTitle.trim(),
+      description: newTaskDesc.trim()
+    }
+
+    setTasks([newTask, ...tasks]);
+    setNewTaskTitle('');
+    setNewTaskDesc('');
+
+
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>My tasks</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder='Enter task title'
+        value={newTaskTitle}
+        onChangeText={setNewTaskTitle}
+        autoCorrect={false}
+      />
+      <TextInput
+        style={[styles.input, styles.inputDesk]}
+        placeholder='Optional description'
+        value={newTaskDesc}
+        onChangeText={setNewTaskDesc}
+        multiline={true}
+        numberOfLines={3}
+        autoCorrect={false}
+      />
+      <Button title='Add task' onPress={addTask} disabled={!newTaskTitle.trim()} />
       {tasks.length === 0 ? <Text style={styles.noTasks}>All tasks have done</Text> 
         : 
       <FlatList
@@ -61,5 +96,16 @@ const styles = StyleSheet.create({
     color: '#888',
     textAlign: 'center',
     marginTop: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 8,
+    borderRadius: 6,
+    marginBottom: 12,
+  },
+  inputDesk: {
+    minHeight: 60,
+    textAlignVertical: 'top'
   }
 })
